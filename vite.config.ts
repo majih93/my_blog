23372045888dd457f6e2b2425003366cada0ffef
jamesma <t90laser93@gmail.com>
2023-07-30 23:26:11 +0900

    [환경설정] vite 절대경로에 components 추가
    -

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+    ],
   },
 });
