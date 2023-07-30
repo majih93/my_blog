a159949a8a255fea54d09b7775a307d8595534e4
jamesma <t90laser93@gmail.com>
2023-07-30 23:27:57 +0900

    [Routing] RouteProvider 및 Root route 추가
    -

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Root from './routes/Root';
+
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/Root.tsx
@@ -0,0 +1,7 @@
+import Layout from '@components/layout/Layout';
+
+const Root = () => {
+  return <Layout />;
+};
+
+export default Root;
