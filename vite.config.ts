725bbd9823acfdb07e028184c1a3eb1f88c74a02
jamesma <t90laser93@gmail.com>
2023-08-06 17:54:18 +0900

    [style] NotoSansKR 폰트 전체 적용
    -

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,10 @@ export default defineConfig({
         find: '@styles',
         replacement: path.resolve(__dirname, 'src/styles'),
       },
+      {
+        find: '@assets',
+        replacement: path.resolve(__dirname, 'src/assets'),
+      },
     ],
   },
 });
