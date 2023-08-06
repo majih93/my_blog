97b19f6ee09547cacab2b27f970df421fcd370f1
jamesma <t90laser93@gmail.com>
2023-08-06 18:42:24 +0900

    [routing] 카테고리 / About me 컴포넌트 및 라우팅 구조 추가
    -

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -2,12 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 
 import Layout from '@/components/layout/DefaultLayout';
 import HomePage from '@/pages/HomePage/HomePage';
+import Categories from '@/pages/Categories/Categories';
+import AboutMe from '@/pages/AboutMe/AboutMe';
 
 export default function Root() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<HomePage />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/about-me" element={<AboutMe />} />
       </Route>
     </Routes>
   );
