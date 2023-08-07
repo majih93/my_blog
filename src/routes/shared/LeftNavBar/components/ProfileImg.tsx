b0c17c7f68ca0088e49dd6dabdc2ab38056c6b35
jamesma <t90laser93@gmail.com>
2023-08-07 23:37:16 +0900

    [LNB][fix] img 태그에 alt 속성값 추가
    -

--- src/routes/shared/LeftNavBar/components/ProfileImg.tsx
+++ src/routes/shared/LeftNavBar/components/ProfileImg.tsx
@@ -3,5 +3,5 @@ import styles from './profileImg.module.scss';
 import Img_Code from '@assets/images/img_code.jpg';
 
 export default function ProfileImg() {
-  return <img src={Img_Code} className={styles.container} />;
+  return <img src={Img_Code} className={styles.container} alt="블로그 저자의 프로필 사진" />;
 }
