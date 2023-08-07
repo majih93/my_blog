ce2f1aaca154fbedab910c2345da78e5df1029c5
jamesma <t90laser93@gmail.com>
2023-08-08 00:28:20 +0900

    [LNB][style] Footer에 깃허브 아이콘 추가 및 링크 연결
    -

--- src/routes/shared/LeftNavBar/components/NavFooter.tsx
+++ src/routes/shared/LeftNavBar/components/NavFooter.tsx
@@ -1,5 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 import styles from './navFooter.module.scss';
 
+const GITHUB_URL = 'https://github.com/majih93';
+
 export default function NavFooter() {
-  return <footer className={styles.container}>하단 footer</footer>;
+  return (
+    <footer className={styles.container}>
+      <a href={GITHUB_URL} target="_blank" rel="nonreferrer">
+        <FontAwesomeIcon icon={faGithub} size="3x" />
+      </a>
+    </footer>
+  );
 }
